822b0df0b46369d51dd8c1652ac87206d26dd01f
kdpkst <jammiechen0@gmail.com>
2023-11-23 10:25:14 +0800

    update boolean4.c

--- learning-resources/c/boolean-usages/boolean4.c
+++ learning-resources/c/boolean-usages/boolean4.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// create a new data type named bool using an enumeration
+// define two constants true and false, which are equivalent to the integer values 1 and 0
 typedef enum { false, true } bool;
 
 int main(void){
